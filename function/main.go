9cabbf13673c2362703f8dca30d64a6de7cf54b4
Amit Masram <amitmasram831@gmail.com>
2024-06-13 22:37:31 +0530

    function

--- function/main.go
+++ function/main.go
@@ -2,17 +2,25 @@ package main
 
 import "fmt"
 
-func simpleFunction(){
+func simpleFunction() {
 	fmt.Println("simple function")
 }
 
-func add(a,b int)(int){
-	return a+b
+func add(a, b int) int {
+	return a + b
 }
+func multiplication(a, b int) (result int) {
+	result = a * b
+	return
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 
 	simpleFunction()
-	ans := add(1,2)
+	ans := add(1, 2)
+	fmt.Println(ans)
+
+	ans = multiplication(1, 2)
 	fmt.Println(ans)
 }
